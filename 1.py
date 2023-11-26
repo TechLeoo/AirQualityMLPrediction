4e992b443f686fb361e80d11345aa8cbd7d6c0ca
Onyiriuba Leonard <leonardleo110011@gmail.com>
2023-11-26 14:41:24 +0100

    Summary

---
+++ 1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 21:13:44 2023
+
+@author: lEO
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("C:/Users/lEO/Desktop/Contract Job/AirQuality.csv")
+a = dataset.columns
